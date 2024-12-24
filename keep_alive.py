e59daac950eb9ffcc24f330f793881865949e9fa
Gupta_A <83379728+neur0n-7@users.noreply.github.com>
2024-12-23 16:23:58 -0800

    Update keep_alive.py
    reverted to self ping

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,20 @@
-from flask import Flask
-from threading import Thread
-import os
-import time
-import requests
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Hyperion is online."
-
 def run():
   app.run(host='0.0.0.0',port=os.environ.get('PORT', 8080))
 
+def self_ping():
+    while True:
+        try:
+            response = requests.get("https://hyperion-z4wx.onrender.com/")
+            if response.status_code == 200:
+                print("Self ping successful")
+            else:
+                print("Ping failed with status code:", response.status_code)
+        except requests.exceptions.RequestException as e:
+            print("Ping failed:", e)
+        time.sleep(5)
+def start_self_ping():
+    s = Thread(target=self_ping)
+    s.start()
 def keep_alive():
     t = Thread(target=run)
     t.start()
